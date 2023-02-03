535b9148db0bb12f08ac8a806f0489b8204d49a2
Yanhao Yang <yangyanh@oregonstate.edu>
2023-02-03 14:08:34 -0800

    Update camera

--- line_tracking/camera_feedback.py
+++ line_tracking/camera_feedback.py
@@ -32,84 +32,6 @@ def region_of_interest(image):
     return masked_image
 
 
-def average_slope_intercept(frame, line_segments):
-    # Create line list
-    lane_lines = []
-    if line_segments is None:
-        logging.info('No line segments detected')
-        return lane_lines
-
-    # Get shape
-    _, width, _ = frame.shape
-
-    # Init lists
-    left_fit = []
-    left_weight = []
-    right_fit = []
-    right_weight = []
-
-    # Take 2/3 of the screen on each side
-    boundary = 1/3
-    left_region_boundary = width * (1 - boundary)
-    right_region_boundary = width * boundary
-
-    for line_segment in line_segments:
-        for x1, y1, x2, y2 in line_segment:
-            # Skip vertical lines
-            if x1 == x2:
-                logging.info('Skipping vertical line segment (slope=inf): %s' % line_segment)
-                continue
-
-            # Compute line parameter
-            fit = np.polyfit((x1, x2), (y1, y2), 1)
-            slope = fit[0]
-            intercept = fit[1]
-            length = np.sqrt((x1-x2)**2 + (y1-y2)**2)
-
-            # Determine left or right by slope
-            if slope < 0:
-                if x1 < left_region_boundary and x2 < left_region_boundary:
-                    left_fit.append((slope, intercept))
-                    left_weight.append(length)
-            else:
-                if x1 > right_region_boundary and x2 > right_region_boundary:
-                    right_fit.append((slope, intercept))
-                    right_weight.append(length)
-
-    # Compute the weighted average
-    left_fit_average = np.average(left_fit, axis=0, weights=left_weight)
-    if len(left_fit) > 0:
-        lane_lines.append(make_points(frame, left_fit_average))
-
-    # Compute the weighted average
-    right_fit_average = np.average(right_fit, axis=0, weights=right_weight)
-    if len(right_fit) > 0:
-        lane_lines.append(make_points(frame, right_fit_average))
-
-    return lane_lines
-
-
-def compute_steering_angle(frame, lane_lines):
-    height, width, _ = frame.shape
-    if not len(lane_lines) == 2:
-        # Check lane number
-        logging.info('Wrong lane number')
-        return 0
-    else:
-        # Compute center
-        _, _, left_x2, _ = lane_lines[0][0]
-        _, _, right_x2, _ = lane_lines[1][0]
-        camera_mid_offset_percent = 0.0
-        mid = int(width / 2 * (1 + camera_mid_offset_percent))
-        x_offset = (left_x2 + right_x2) / 2 - mid
-
-    # Compute steering angle
-    y_offset = int(height / 2)
-    steering_angle = np.rad2deg(math.atan(x_offset / y_offset))
-
-    return steering_angle
-
-
 def display_lines(frame, lines, line_color=(0, 255, 0), line_width=10):
     # Copy frame
     line_frame = np.copy(frame)
@@ -143,22 +65,6 @@ def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_wid
     return heading_image
 
 
-def make_points(frame, line):
-    # Get dimension
-    height, width, _ = frame.shape
-
-    # Get line parameter
-    slope, intercept = line
-
-    # Compute points
-    y1 = height
-    y2 = int(y1 * 1 / 2)
-    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
-    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
-
-    return [[x1, y1, x2, y2]]
-
-
 def camera_feedback(frame, plot=False):
     if plot:
         cv2.imshow('frame', frame)
@@ -188,31 +94,28 @@ def camera_feedback(frame, plot=False):
     if plot:
         cv2.imshow('closing', closing)
 
-    # Detect edges
-    edges = cv2.Canny(closing, 100, 200)
-    if plot:
-        cv2.imshow('edges', edges)
-
     # Crop ROI
-    cropped_edges = region_of_interest(edges)
+    cropped_closing = region_of_interest(closing)
     if plot:
-        cv2.imshow('cropped_edges', cropped_edges)
+        cv2.imshow('cropped_closing', cropped_closing)
 
-    # Fit lines
-    line_segments = cv2.HoughLinesP(cropped_edges, 1, np.pi / 180, 25, None, 50, 75)
-    line_segment_image = display_lines(frame, line_segments)
-    if plot:
-        cv2.imshow("line_segment_image", line_segment_image)
+    # Detect contours
+    contours, _ = cv2.findContours(cropped_closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    big_contour = max(contours, key=cv2.contourArea)
 
-    # Merge lines
-    lane_lines = average_slope_intercept(frame, line_segments)
-    lane_lines_image = display_lines(frame, lane_lines)
+    contours_img = np.copy(frame)
+    cv2.drawContours(contours_img, [big_contour], 0, (0, 0, 255), 2)
     if plot:
-        cv2.imshow("lane_lines_image", lane_lines_image)
+        cv2.imshow('contours_img', contours_img)
 
     # Compute steering
-    steering_angle = compute_steering_angle(frame, lane_lines)
-    final_frame = display_heading_line(lane_lines_image, steering_angle)
+    M = cv2.moments(big_contour)
+    cx = M['mu20']/M['m00']
+    cy = M['mu02']/M['m00']
+    cxy = M['mu11']/M['m00']
+    steering_angle = np.round(1/2*np.arctan(2*cxy/(cx-cy)))
+
+    final_frame = display_heading_line(frame, steering_angle)
     logging.info('steering_angle: %d' % steering_angle)
     cv2.imshow("final_frame", final_frame)
 
@@ -248,7 +151,7 @@ px.run(50)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     frame = frame.array
     steering_angle_current = camera_feedback(frame)
-    steering_angle = steering_angle*0.5 + steering_angle_current*0.5
+    steering_angle = steering_angle*0.9 + steering_angle_current*0.1
     px.turn(np.round(steering_angle))
     rawCapture.truncate(0)   # Release cache
 
